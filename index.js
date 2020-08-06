f5257b64234d50a22ca3070a83e02a1f4c38fad2
savage <nawedanjum1111@gmail.com>
2020-08-07 01:00:03 +0530

    second

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ import express = require 'express';
 
 const app = express();
 
+app.get('/',(res,req) => {
+	res.send({hi:'there'});
+}); 
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
