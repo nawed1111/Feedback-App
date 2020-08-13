d11e4b5f99ff59a707c18dd24f1d2f1e66af3744
savage <nawedanjum1111@gmail.com>
2020-08-14 02:27:52 +0530

    Added paytment

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-const App = () => {
-	return (
-		<div>
-			Hi There!
-		</div>
-	);
+import Header from './Header';
+import Landing from './Landing';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+const Dashboard = () => <h2> Dashboard </h2>;
+const SurveyNew = () => <h2> SurveyNew </h2>;
+
+class App extends Component{
+	
+	componentDidMount(){
+
+		this.props.fetchUser();
+	}
+
+	render(){
+		return (
+			<div>
+				<BrowserRouter> 
+					<div>
+						<Header />
+						<Route exact path="/" component={Landing} /> 
+						<Route exact path="/surveys" component={Dashboard} />
+						<Route path="/surveys/new" component={SurveyNew} /> 
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
 }
 
-export default App;
+export default connect(null, actions)(App);
